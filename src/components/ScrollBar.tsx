8e50ec4bb9b8521c6974040aaa0a994091c0b068
Jaeseong-CS <shimjs8@naver.com>
2020-10-27 06:44:25 +0900

    fix: more pleasant user experience

--- src/components/ScrollBar.tsx
+++ src/components/ScrollBar.tsx
@@ -1,15 +1,17 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 const ScrollBarWrapper = styled.div`
   position: fixed;
-  width: 0.3em;
+  display: flex;
+  width: 0.5em;
   height: calc(100vh - 2.2em);
   top: 2.2em;
   right: 0;
   z-index: 99;
   opacity: 0.7;
   transition: 0.5s ease-out;
+  justify-content: center;
 
   @media screen and (max-width: 1024px) {
     display: none;
@@ -28,6 +30,7 @@ const ScrollBarIndicator = styled.div`
 const ScrollBar: React.FC = () => {
   const scrollBarWrapper = useRef<HTMLDivElement>(null);
   const scrollBarIndicator = useRef<HTMLDivElement>(null);
+  const [hover, setHover] = useState(true);
 
   useEffect(() => {
     let timer = 0;
@@ -38,22 +41,23 @@ const ScrollBar: React.FC = () => {
       const element = document.documentElement;
       const wScroll = element.scrollTop / 3;
       const wHeight = (element.scrollHeight - element.clientHeight) / 3;
-      scrollBarIndicator.current!.style.top = `calc(${wScroll}px + 2.2em)`;
+      scrollBarIndicator.current!.style.top = `calc(${wScroll}px + 2.3em)`;
       scrollBarIndicator.current!.style.height = `calc(${
         (1 - wHeight / element.clientHeight) * 100
-      }vh - 2.2em)`;
+      }vh - 2.4em)`;
       ((t) => {
         setTimeout(() => {
-          if (timer === t) {
+          if (hover && timer === t) {
             scrollBarWrapper.current!.style.opacity = '0';
           }
-        }, 1000);
+        }, 500);
       })(timer);
     };
 
+    window.addEventListener('load', setScrollBar);
     window.addEventListener('scroll', setScrollBar);
     window.addEventListener('resize', setScrollBar);
-  }, []);
+  }, [hover]);
 
   const mouseMove = (e: MouseEvent) => {
     document.documentElement.scrollTop += e.movementY * 3;
@@ -62,9 +66,11 @@ const ScrollBar: React.FC = () => {
   return (
     <ScrollBarWrapper
       onMouseEnter={() => {
+        setHover(true);
         scrollBarWrapper.current!.style.opacity = '0.7';
       }}
       onMouseLeave={() => {
+        setHover(false);
         scrollBarWrapper.current!.style.opacity = '0';
       }}
       ref={scrollBarWrapper}

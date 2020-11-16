f73486739a428add08096f8c8df8fec0336d5e8b
Jaeseong Sim <shimjs8@naver.com>
2020-11-16 16:48:10 +0900

    feat: design

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -1,15 +1,26 @@
 import { TextField, TextFieldProps } from '@material-ui/core';
 import React from 'react';
+import styled from 'styled-components';
+
+const TextFieldEx = styled(TextField)`
+  .MuiInput-underline:after {
+    border-bottom-color: #ffc7c7 !important;
+  }
+
+  .Mui-focused {
+    color: #ffc7c7 !important;
+  }
+`;
 
 const TextBox: React.FC<TextFieldProps> = (props: TextFieldProps) => {
   const { inputProps, InputLabelProps, ...other } = { ...props };
 
   return (
-    <TextField
+    <TextFieldEx
       inputProps={{
         style: {
           fontFamily: 'Hi Melody',
-          color: '#1a1a1a',
+          color: '#5b3f40',
         },
       }}
       InputLabelProps={{
